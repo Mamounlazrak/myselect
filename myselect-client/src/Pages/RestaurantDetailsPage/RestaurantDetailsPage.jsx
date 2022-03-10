0dac21d45f78545f64f3e44a89cb6f53c010edbb
Mamoun Lazrak <mamounlazrak@gmail.com>
2022-03-10 22:19:57 +0000

    fixed add to my list

--- myselect-client/src/Pages/RestaurantDetailsPage/RestaurantDetailsPage.jsx
+++ myselect-client/src/Pages/RestaurantDetailsPage/RestaurantDetailsPage.jsx
@@ -8,22 +8,32 @@ function RestaurantDetailsPage() {
  
     const { restaurantId } = useParams();
     const [restaurant, setRestaurant] = useState(null);
-    const { loggedIn, user, logoutUser } = useContext(AuthContext);
+    const { loggedIn, user, logoutUser, authenticateUser } = useContext(AuthContext);
     const [myRestaurants, setMyRestaurants] = useState(null);
 
 
+    const fetchMyRestaurants = async () => {
+        try {
+            let response = await axios.get(`${process.env.REACT_APP_API_URL}/api/myrestaurants/no-populate/${user._id}`);
+            setMyRestaurants(response.data);
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
     const fetchRestaurant = async () => {
         try{
             let response = await axios.get(`${process.env.REACT_APP_API_URL}/api/restaurants/${restaurantId}`)
             setRestaurant(response.data);
-            // refreshMyRestaurants
+            fetchMyRestaurants();
             // setMyRestaurants(user.restaurantsList);
         } catch (error) {
             console.log(error);
         }
     };
 
-    // const fetchMyRestaurants = () => {}
+
+
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -31,19 +41,23 @@ function RestaurantDetailsPage() {
         axios
             .put(`${process.env.REACT_APP_API_URL}/api/myrestaurants/${user._id}/${restaurantId}`)
             .then((newList) => {
-                setMyRestaurants(newList.data)
-                user.restaurantsList = newList.data})
+                // setMyRestaurants(newList.data)
+                // user.restaurantsList = newList.data
+                authenticateUser()})
             // .then((newList) => console.log(newList))
             // console.log(myRestaurants)
             .catch((err) => console.log(err));
     }
     useEffect( () => {fetchRestaurant();
     }, []);
+    useEffect( () => {fetchMyRestaurants();
+    }, [myRestaurants]);
 
   return (
     <div>
         {/* {console.log('HEYYY', myRestaurants)} */}
         {restaurant && <h3>{restaurant.name}</h3>}
+        {console.log(myRestaurants)}
         <form onSubmit={handleSubmit}>
             {/* { loggedIn && user.restaurantsList.includes(restaurantId) ? <button type='submit'>Remove from my list</button> : <button type='submit'>Add to my list</button> } */}
             { myRestaurants && myRestaurants.includes(restaurantId) ? <button type='submit'>Remove from my list</button> : <button type='submit'>Add to my list</button> }
